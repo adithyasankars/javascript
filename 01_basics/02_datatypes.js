a26f5eb2980fb5474a734b0258a20c9a2f4a2f83
adithyasankars <167020861+adithyasankars@users.noreply.github.com>
2024-04-16 04:47:17 +0000

    Dataypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+// "use strict"; // treats all JS code as newer version.
+
+// alert("hello");
+
+// alert(3+3);  we are using node and browser.
+
+
+let name = "adithya"
+let age = 18
+let isLoggedIn = false
+
+
+// P R I M I T I V E  D A T A T Y P E S
+
+// number ==> 2 to the power 53
+// bigint
+// string ==> ""
+// boolean  ==> true/false
+// null ==> standalone value
+// undefined ==> for now the value haven't been assigned.
+// symbol ==> unique
+
+console.log(typeof name);
+
+
+// P  D A T A T Y P E S
