7d9bc7d037074e2be76051c5955949a357cdecca
adithyasankars <167020861+adithyasankars@users.noreply.github.com>
2024-04-16 05:56:54 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_datatypeConfusion.js
@@ -0,0 +1,122 @@
+// let score = "33"
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+// node 01_basics/03_datatypeConfusion.js
+
+// let valueInNUmber = Number(score);
+
+// console.log(typeof valueInNUmber);
+
+
+// ----------------------------------------------------------------------------------------------------------------
+
+
+// let score = "33abc"
+
+// console.log(typeof score);
+
+// let valueInNUmber = Number(score);
+
+// console.log(typeof valueInNUmber);
+
+// console.log(valueInNUmber);
+
+// ----------------------------------------------------------------------------------------------------------------
+
+
+// let score = null
+
+// console.log(typeof score);
+
+// let valueInNUmber = Number(score);
+
+// console.log(typeof valueInNUmber);
+
+// console.log(valueInNUmber);
+
+// ---------------------------------------------------------------------------------------------------------------
+
+// let score = undefined
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// ---------------------------------------------------------------------------------------------------------------
+
+// let score = true
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// ---------------------------------------------------------------------------------------------------------------
+
+// let score = false
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// ---------------------------------------------------------------------------------------------------------------
+
+// let score = "Adithya"
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33acd" => NaN
+// true => 1; false => 0
+
+
+
+
+
+// ---------------------------------------------------------
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn  = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// node 01_basics/03_datatypeConfusion.js
+
+// ---------------------------------------------------------
+
+// let isLoggedIn = 0
+
+// let booleanIsLoggedIn  = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// -----------------------------------------------------------
+
+// let isLoggedIn = "Adithya"
+
+// let booleanIsLoggedIn  = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// -----------------------------------------------------------
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn  = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false;
+// "" => false
+// "Adithya" => true
+
