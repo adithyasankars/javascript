070bd319611bcc026fcacacb0c4466dbdc328075
adithyasankars <167020861+adithyasankars@users.noreply.github.com>
2024-04-16 10:25:59 +0000

    Summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,65 @@
+// ======================================== S U M M A R Y ==============================================
+
+// there are two types of data types
+
+//  P R I M I T I V E
+//  based on how they are stored and how they are accessed.
+
+// they are 0f 7 : String, Number , boolean , null , undefined , symbol, bigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIN = false
+const oustsideTemp = null
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+// console.log(id === anotherId);
+
+//const bigNumber = 49457845098257028579025634n;
+
+// console.log(typeof bigNumber);
+
+
+// R E F E R N C E (NON - PRIMITIVE)
+
+// they are of 3 : Array, Objects, Functions.
+
+
+const heros = ["Ironman ", "Captain America", "Minnal Murali"];
+
+let myObj = {
+    name: "Adithya",
+    age: 23
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNUmber);
+
+// node 01_basics/05_datatypes_summary
+
+
+/* 
+   
+   Number => number
+   String => string
+   Boolean => boolean
+   null => object
+   undefined => undefined
+   Symbol => symbol
+   bigInt => bigint
+
+
+   Arrays => Object
+   Function => function
+   Object => object
+
+
+*/
+   
