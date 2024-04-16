a107cc113b454359e1cd472b49e75ad963721152
adithyasankars <167020861+adithyasankars@users.noreply.github.com>
2024-04-16 09:13:18 +0000

    Operations in JS

--- 01_basics/03_datatypeConfusion.js
+++ 01_basics/03_datatypeConfusion.js
@@ -120,3 +120,51 @@
 // "" => false
 // "Adithya" => true
 
+
+
+// ============================================================================ O P E R A T I O N S ====================================================
+
+// node 01_basics/03_datatypeConfusion.js
+
+let value = 3;
+let negValue = -value;
+
+//console.log(negValue);
+
+// ------------------------------------------------------------------------------------------
+
+//console.table([2+3, 2-3, 2*3, 2/3, 2**3, 2%3]);
+
+// ------------------------------------------------------------------------------------------
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(true+);
+// console.log(+"");
+
+
+let num1, num2 , num3;
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
