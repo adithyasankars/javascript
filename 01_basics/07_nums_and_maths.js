9020d21e876566956bcf6b04e6ce0ac2dfb23896
adithyasankars <167020861+adithyasankars@users.noreply.github.com>
2024-04-17 04:33:44 +0000

    Numbers and Maths

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,55 @@
+const score = 400;
+
+const balance = new Number(100);
+
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// console.log(typeof (balance));
+
+const num1 = 35.566632;
+
+// console.log(num1.toPrecision(4));
+
+// ===========================================================================================================================
+
+const hundreds = 100000000000;
+
+// console.log(hundreds.toLocaleString()); // US STANDARD
+
+// console.log(hundreds.toLocaleString('en-IN')); // Indian STANDARD
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++ M A T H S +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// ----------------------------------------------- console.log(Math); --------------------------------------------------------
+
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+// console.log(Math.round(56.74));
+// console.log(Math.ceil(56.74));
+// console.log(Math.floor(56.74));
+// console.log(Math.min(3,5,67,43));
+// console.log(Math.max(3,45,1,3,445));
+
+
+// ------------------------------------------------- dice --------------------------------------------------------------
+
+// console.log(Math.random());
+
+// console.log((Math.random()*10) + 1);
+
+const min = 20;
+const max = 30;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+// node 01_basics/07_nums_and_maths.js
