5d27bd1a6e396f53058943c40436eb5642ed92fc
adithyasankars <167020861+adithyasankars@users.noreply.github.com>
2024-04-18 05:52:54 +0000

    learned scope - block and global

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,23 @@
+// ----------------------------------------------------- S C O P E -----------------------------------------------------------
+
+// let a = 10;
+// const b = 20;
+// var c = 30;
+
+let a = 300;
+
+if (true) {
+
+    let a = 10;
+    const b = 20;
+//  var c = 30;                    // scope
+}
+
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// node 03_basics/02_scope.js
