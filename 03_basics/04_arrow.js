001e9da03aaa70d7d49b70bcc39d9ddddae78aaa
adithyasankars <167020861+adithyasankars@users.noreply.github.com>
2024-04-18 09:31:26 +0000

    Arrows -implicit

--- 03_basics/04_arrow.js
+++ 03_basics/04_arrow.js
@@ -2,17 +2,71 @@
 
 // -----------------------------------------------------------arrow-------------------------------------------
 
-const user = {
-    userName : "Adithya",
-    price:999,
+// const user = {
+//     userName : "Adithya",
+//     price:999,
 
-    welcomeMessage : function(){
-        console.log(`${this.userName} , Welcome to website.`);
-    }
-}
+//     welcomeMessage : function(){
+//         console.log(`${this.userName} , Welcome to website.`);
+//     }
+// }
 
 // current context
 
-user.welcomeMessage();
-user.userName = "Adithya";
-user.welcomeMessage();
+// user.welcomeMessage();
+// user.userName = "Adithya";
+// user.welcomeMessage();
+
+// const chai = function(){
+//     let userName = "Adithya";
+//     console.log(this.userName);
+// }
+
+// chai();
+
+// -------------output --- undefined
+
+// const chai = () => {
+//     let userName ="Adithya";
+//     console.log(this.userName);
+
+// }
+
+// chai()
+
+// -------------output --- undefined
+
+// const chai = () => {
+//     let userName ="Adithya";
+//     console.log(this);
+
+// }
+
+// chai();
+
+// -------------output ---- { } ----empty paranthesis
+
+
+// () => {}
+
+// const addTwo = (num1,num2) => {
+//     return num1 + num2;                         // explicit when u use return
+// }
+
+// console.log(addTwo(3,4));
+
+// ----------------------------basic  arrow function
+
+// const addTwo = (num1,num2) => num1 + num2 // implicit return
+
+// const addTwo = (num1,num2) => (num1 + num2) // if you use {} then you have to use return and
+                                            //if u avoid the { } then you can use ()
+
+// const addTwo = (num1,num2) => ({username:"Adithya"}) // object
+
+// console.log(addTwo(3,4));
+
+
+
+
+// node 03_basics/04_arrow.js
